77830fd263b9ed2464fe670973dd7a21e51d59fe
esau-morais <esaumorais7@gmail.com>
2023-03-09 13:00:35 -0300

    fix: update classNames and add readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# Stacked Cards
+
+Cool glowing cards effect inspired by https://linear.app/features
+
+![glowingCards showcase GIF](https://github.com/esau-morais/glowingCards.ts/blob/main/public/Glowing-Cards.gif)
+
+## 🛠️ Setup
+
+1. Clone the project
+
+```
+gh repo clone esau-morais/glowingCards.ts
+```
+
+3. Installing (using `pnpm`)
+
+```
+pnpm install 
+```
+
+4. Running (using `pnpm`)
+
+```
+pnpm dev
+```
+
+## ✏️ LICENSE
+
+Check the `LICENSE` file at the root of the project
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,12 @@ module.exports = {
       fontFamily: {
         'space-grotesk': ["'Space Grotesk'", ...fontFamily.sans],
       },
+      backgroundImage: {
+        'gradient-before':
+          'radial-gradient(800px circle at var(--mouse-x) var(--mouse-y), rgba(255, 255, 255, 0.06), transparent 40%)',
+        'gradient-after':
+          'radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(255, 255, 255, 0.4), transparent 40%)',
+      },
     },
   },
   plugins: [],
